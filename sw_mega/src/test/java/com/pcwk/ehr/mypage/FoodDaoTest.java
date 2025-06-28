39ea9ce9d3d0a4c8eaf362ff85ffdbdf90c1c594
YangSeungHyunn <yangsh5972@gmail.com>
2025-06-28 16:24:50 +0900

    foodDaoTest all clear

--- sw_mega/src/test/java/com/pcwk/ehr/mypage/FoodDaoTest.java
+++ sw_mega/src/test/java/com/pcwk/ehr/mypage/FoodDaoTest.java
@@ -11,10 +11,12 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -50,9 +52,9 @@ class FoodDaoTest {
 		log.debug("│ setUp()                                                 │");
 		log.debug("└─────────────────────────────────────────────────────────┘");
 		
-		dto01 = new FoodDTO("김치볶음밥", 200, 430, 30, 20, 20,30);
-		dto02 = new FoodDTO("된장찌개", 200, 430, 30, 20, 20,30);
-		dto03 = new FoodDTO("삽겹살", 200, 430, 30, 20, 20,30);
+		dto01 = new FoodDTO("김치볶음밥", 200, 430, 30, 20, 20, 30);
+		dto02 = new FoodDTO("된장찌개", 200, 430, 30, 20, 20, 30);
+		dto03 = new FoodDTO("삽겹살", 200, 430, 30, 20, 20, 30);
 		
 		search = new SearchDTO();
 		
@@ -65,6 +67,106 @@ class FoodDaoTest {
 		log.debug("└─────────────────────────────────────────────────────────┘");
 	}
 	
+	@Disabled
+	@Test
+	void doDelete() throws SQLException{
+		log.debug("┌─────────────────────────────────────────────────────────┐");
+		log.debug("│ doDelete()                                              │");
+		log.debug("└─────────────────────────────────────────────────────────┘");
+		
+		//1. 전체삭제
+		//2. 단건등록
+		//3. 삭제
+		//4. 등록 건수 조회
+		
+		//1. 
+		mapper.deleteAll();
+		
+		//2. 
+		mapper.doSave(dto01); 
+		
+		//3. 
+		FoodDTO param = new FoodDTO();
+		param.setFoodName("김치볶음밥");
+		int result = mapper.doDelete(param);
+		
+		assertEquals(1, result);
+		
+		//4.
+		int count = mapper.getCount();
+		log.debug(count);
+		
+	}
+	
+	//@Disabled
+	@Test
+	void doUpdate() throws SQLException{
+		log.debug("┌─────────────────────────────────────────────────────────┐");
+		log.debug("│ doUpdate()                                              │");
+		log.debug("└─────────────────────────────────────────────────────────┘");
+		
+		//1. 전체삭제
+		//2. 단건등록
+		//3. 단건조회 및 값 변경
+		//4. 수정하기
+		
+		//1. 
+		mapper.deleteAll();
+		
+		//2. 
+		mapper.doSave(dto01); 
+		
+		//3. 
+		FoodDTO existing = mapper.doSelectOne(dto01);
+		existing.setCal(50); //cal 값만 변경
+		
+//		FoodDTO param = new FoodDTO();
+//		param.setFoodName("김치볶음밥");
+//		param.setCal(20);
+		
+		//4.
+		int result = mapper.doUpdate(existing);
+		
+		assertEquals(1, result);
+		
+	}
+	
+	@Disabled
+	@Test
+	void doSelectOne()throws SQLException{
+		log.debug("┌─────────────────────────────────────────────────────────┐");
+		log.debug("│ doSelectOne()                                           │");
+		log.debug("└─────────────────────────────────────────────────────────┘");
+		
+		//1. 전체삭제
+		//2. 단건등록 x 3
+		//2.1 등록 건수 비교
+		//3. 단건조회
+		
+		//1. 
+		mapper.deleteAll();
+		
+		//2. 
+		mapper.doSave(dto01); 
+		mapper.doSave(dto02);
+		mapper.doSave(dto03);
+		
+		//2.1 등록 건수 비교
+		int count = mapper.getCount();
+		assertEquals(3, count);
+		log.debug(count);
+		
+		//3. 
+		FoodDTO param = new FoodDTO(); 
+		param.setFoodName("된장찌개");
+		FoodDTO result = mapper.doSelectOne(param);
+		
+		assertEquals("된장찌개", result.getFoodName());
+		
+		
+	}
+	
+	@Disabled
 	@Test
 	void doRetrieve() throws SQLException{
 		log.debug("┌─────────────────────────────────────────────────────────┐");
@@ -99,6 +201,7 @@ class FoodDaoTest {
 		}
 		
 	}
+	
 
 	@Test
 	void beans() {
