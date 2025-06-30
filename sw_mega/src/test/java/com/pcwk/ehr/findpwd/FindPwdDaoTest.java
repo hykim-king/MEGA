609c4a47a5e63d53c2cce38ebeb5c05ed4b206a3
subtang <wdh142@gmail.com>
2025-06-30 15:24:02 +0900

    DaoTestComplete

---
+++ sw_mega/src/test/java/com/pcwk/ehr/findpwd/FindPwdDaoTest.java
@@ -0,0 +1,35 @@
+package com.pcwk.ehr.findpwd;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.pcwk.ehr.login.dao.FindPwdDao;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = {
+    "file:src/main/webapp/WEB-INF/spring/root-context.xml",
+    "file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"
+})
+public class FindPwdDaoTest {
+	Logger log=LogManager.getLogger(getClass());
+
+    @Autowired
+    FindPwdDao findPwdDao;
+    
+    @Autowired
+	ApplicationContext context;
+
+    @Test
+    void daoNotNullTest() {
+        assertNotNull(findPwdDao, "FindPwdDao가 정상적으로 주입되어야 합니다.");
+        log.debug("context: "+context);
+    }
+}
