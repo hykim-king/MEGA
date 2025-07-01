2a9ea8f27952d78ef73c0b30800ed996589250a3
pcwk <dlqud357@gmail.com>
2025-07-01 22:30:17 +0900

    board project

--- sw_mega/src/test/java/com/pcwk/ehr/board/NoticeDTOTest.java
+++ sw_mega/src/test/java/com/pcwk/ehr/board/NoticeDTOTest.java
@@ -22,6 +22,8 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import com.pcwk.ehr.board.domain.NoticeDTO;
 import com.pcwk.ehr.board.mapper.NoticeMapper;
 import com.pcwk.ehr.cmn.SearchDTO;
+import com.pcwk.ehr.mypage.domain.ExerciseDTO;
+import com.pcwk.ehr.mypage.domain.FoodDiaryDTO;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/root-context.xml",
@@ -62,46 +64,163 @@ class NoticeDTOTest {
 		log.debug("│ tearDown()                 │");
 		log.debug("└────────────────────────────┘");
 	}
+	//@Disabled
+	@Test
+	void viewCount() {
+		log.debug("┌────────────────────────────┐");
+		log.debug("│ viewCount()                │");
+		log.debug("└────────────────────────────┘");
+		// 전체삭제
+		// 단건등록
+		// 전체조회
+
+		// 1.
+		mapper.deleteAll();
+
+		// 2.
+		mapper.doSave(dto01);
+
+		int count = mapper.getCount();
+		assertEquals(1, count);
+		log.debug("count:{}", count);
+
+		// paging
+		search.setSearchWord(dto01.getTitle());
+		search.setSearchDiv("10");
+		search.setPageSize(10);
+		search.setPageNo(1);
+
+		// 3.
+		List<NoticeDTO> list = mapper.doRetrieve(search);
+		for (NoticeDTO vo : list) {
+			log.debug("vo: {}", vo);
+		}
+
+		// 항목 선택
+		int result = mapper.viewCount(list.get(0));
+		log.debug(result);
+
+		// 조회
+		NoticeDTO result1 = mapper.doSelectOne(list.get(0));
+		log.debug("result1:{}" ,result);
+
+
+	}
+
+	//@Disabled
+	@Test
+	void doSelectOne() {
+		log.debug("┌────────────────────────────┐");
+		log.debug("│ doSelectOne()              │");
+		log.debug("└────────────────────────────┘");
+
+		// 1. 전체삭제
+		// 2. 단건등록
+		// 3.등록건수 조회
+
+		// 1.
+		mapper.deleteAll();
+
+		// 2.
+		mapper.doSave(dto01);
+		mapper.doSave(dto02);
+		mapper.doSave(dto03);
+
+		int count = mapper.getCount();
+		assertEquals(3, count);
+		log.debug("count:{}", count);
+
+		// paging
+		search.setSearchWord(dto01.getTitle());
+		search.setSearchDiv("10");
+		search.setPageSize(10);
+		search.setPageNo(1);
+
+		// 3.
+		List<NoticeDTO> list = mapper.doRetrieve(search);
+		for (NoticeDTO vo : list) {
+			log.debug("vo: {}", vo);
+		}
+
+		// 항목 선택
+		NoticeDTO result = mapper.doSelectOne(list.get(0));
+		log.debug(result);
+	}
+
+	//@Disabled
+	@Test
+	void doUpdate() {
+		log.debug("┌────────────────────────────┐");
+		log.debug("│ doUpdate()                 │");
+		log.debug("└────────────────────────────┘");
+		// 1. 전체삭제
+		// 2. 단건등록
+		// 3. 전체조회 및 항목선택
+		// 4. 수정
+		// 1.
+		mapper.deleteAll();
+
+		// 2.
+		mapper.doSave(dto01);
+		mapper.doSave(dto02);
+		mapper.doSave(dto03);
+
+		int count = mapper.getCount();
+		assertEquals(3, count);
+		log.debug("count: {}", count);
+
+		// 3
+		search.setSearchWord(dto01.getTitle());
+		search.setSearchDiv("10");
+		search.setPageSize(10);
+		search.setPageNo(1);
+		List<NoticeDTO> list = (List<NoticeDTO>) mapper.doRetrieve(search);
+		for (NoticeDTO noVO : list) {
+			log.debug("noVO:{}", noVO);
+		}
+		list.get(0).setTitle("제목1");
+		list.get(0).setContent("내용1");
+		int result = mapper.doUpdate(list.get(0));
+		log.debug(result);
+
+	}
 
 	//@Disabled
 	@Test
 	void doDelete() throws SQLException {
-	    log.debug("┌────────────────────────────┐");
-	    log.debug("│ doDelete()                 │");
-	    log.debug("└────────────────────────────┘");
+		log.debug("┌────────────────────────────┐");
+		log.debug("│ doDelete()                 │");
+		log.debug("└────────────────────────────┘");
 
-	 // 매번 동일한 결과가 도출 되도록 작성
-	 		// 1. 전체삭제
-	 		// 2. 단건등록
-	 		// 2.1 등록건수 비교
-	 		// 3. 삭제
-	 		// 4. 등록건수 비교==0
+		// 매번 동일한 결과가 도출 되도록 작성
+		// 1. 전체삭제
+		// 2. 단건등록
+		// 2.1 등록건수 비교
+		// 3. 삭제
+		// 4. 등록건수 비교==0
 
-	 		// 1.
-	 		mapper.deleteAll();
+		// 1.
+		mapper.deleteAll();
 
-	 		// 2.
-	 		int flag = mapper.doSave(dto01);
-	 		log.debug("flag:{}", flag);
-	 		assertEquals(1, flag);
+		// 2.
+		int flag = mapper.doSave(dto01);
+		log.debug("flag:{}", flag);
+		assertEquals(1, flag);
 
-	 		// 2.1
-	 		int count = mapper.getCount();
-	 		log.debug("count:{}", count);
-	 		assertEquals(1, count);
+		// 2.1
+		int count = mapper.getCount();
+		log.debug("count:{}", count);
+		assertEquals(1, count);
 
-	 		// 3 삭제
-	 		flag = mapper.doDelete(dto01);
-	 		assertEquals(0, flag);
+		// 3 삭제
+		flag = mapper.doDelete(dto01);
+		assertEquals(0, flag);
 
-	 		// 4.건수비교
-	 		count = mapper.getCount();
-	 		log.debug("count:{}", count);
-	 		assertEquals(1, count);
-	 	}
-
-
-
+		// 4.건수비교
+		count = mapper.getCount();
+		log.debug("count:{}", count);
+		assertEquals(1, count);
+	}
 
 	//@Disabled
 	@Test
