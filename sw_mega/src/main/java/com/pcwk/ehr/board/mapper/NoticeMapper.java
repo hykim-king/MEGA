925e95fe18a2780c360b6f608822e319dbec8821
ebh99 <bh990907@naver.com>
2025-07-01 14:53:51 +0900

    board edit

--- sw_mega/src/main/java/com/pcwk/ehr/board/mapper/NoticeMapper.java
+++ sw_mega/src/main/java/com/pcwk/ehr/board/mapper/NoticeMapper.java
@@ -1,10 +1,8 @@
 package com.pcwk.ehr.board.mapper;
 
-import org.apache.ibatis.annotations.Mapper;
-
 import com.pcwk.ehr.board.domain.NoticeDTO;
 import com.pcwk.ehr.cmn.WorkDiv;
 
-@Mapper
+
 public interface NoticeMapper extends WorkDiv<NoticeDTO> {
 }
