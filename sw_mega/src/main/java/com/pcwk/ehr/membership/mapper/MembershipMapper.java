e87f31bd1a8973fb92cc442daba7a54bc76498e6
doona0711 <com0494@naver.com>
2025-07-03 02:44:36 +0900

    membership dao,service,controller test

--- sw_mega/src/main/java/com/pcwk/ehr/membership/mapper/MembershipMapper.java
+++ sw_mega/src/main/java/com/pcwk/ehr/membership/mapper/MembershipMapper.java
@@ -10,33 +10,34 @@ import com.pcwk.ehr.cmn.WorkDiv;
 import com.pcwk.ehr.membership.domain.MembershipDTO;
 
 @Mapper
-public interface MembershipMapper extends WorkDiv<MembershipDTO>{
-	
-    /** ID 중복 체크: 0이면 사용 가능 */
-    int idCheck(String userId) throws SQLException;
+public interface MembershipMapper extends WorkDiv<MembershipDTO> {
 
-    /** 등급 업그레이드용 전체 조회 */
-    List<MembershipDTO> getAll() throws SQLException;
+	/** ID 중복 체크: 0이면 사용 가능 */
+	int idCheck(String userId) throws SQLException;
 
-    /** 목록 조회(페이징) - WorkDiv에 없다면 선언 */
-    List<MembershipDTO> doRetrieve(SearchDTO search);
-	
-    /**
+	/** 등급 업그레이드용 전체 조회 */
+	List<MembershipDTO> getAll() throws SQLException;
+
+	/** 목록 조회(페이징) - WorkDiv에 없다면 선언 */
+	//List<MembershipDTO> doRetrieve(SearchDTO search);
+
+	/**
 	 * 전체삭제
 	 */
-	void deleteAll();
-	
+	void deleteAll() throws SQLException;;
+
 	/**
 	 * 등록 건수 세기
+	 * 
 	 * @return
 	 */
-	int getCount();
-	
-	  /*
-		 * 다건등록
-		 * @return 1(성공)/0(실패)
-		 */
-		int saveAll();
-	
-	
+	int getCount() throws SQLException;
+
+	/*
+	 * 다건등록
+	 * 
+	 * @return 1(성공)/0(실패)
+	 */
+	int saveAll() throws SQLException;
+
 }
