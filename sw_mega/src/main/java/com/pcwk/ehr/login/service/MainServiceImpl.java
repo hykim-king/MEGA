b980574b01e6cc55361c4d603f83dc12d5a38638
subtang <wdh142@gmail.com>
2025-07-09 22:33:38 +0900

    mainpage 임시 학원에서 테스트한번더해봐야함

---
+++ sw_mega/src/main/java/com/pcwk/ehr/login/service/MainServiceImpl.java
@@ -0,0 +1,10 @@
+package com.pcwk.ehr.login.service;
+
+public class MainServiceImpl implements MainService {
+
+	@Override
+	public String getWelcomeMessage() {
+		 return "오늘도 건강한 하루를 HELLMATE와 함께 시작하세요!";
+	}
+
+}
