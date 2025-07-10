caedd4a06ee0e6a60a28336caa630b0ba2229e3d
ebh99 <bh990907@naver.com>
2025-07-10 20:17:01 +0900

    웹 오류 해결 못함

--- sw_mega/src/main/java/com/pcwk/ehr/mapper/L_ReactionMapper.java
+++ sw_mega/src/main/java/com/pcwk/ehr/mapper/L_ReactionMapper.java
@@ -7,11 +7,21 @@ import com.pcwk.ehr.cmn.WorkDiv;
 
 @Mapper
 public interface L_ReactionMapper extends WorkDiv<L_ReactionDTO> {
+
 	
 	/**
-	 * 등록 건수 세기
-	 * @return
+	 * 등록 메서드 등 다른 메서드
+	 * 
 	 */
+	int doSave(L_ReactionDTO dto);
+	
+	/**
+	 * 좋아요/싫어요 수 조회용
+	 * 
+	 */
+	int getCount(L_ReactionDTO dto);
+	
+	//DAOTEST GETCOUNT빨간줄 제거용
 	int getCount();
 	
 	/**
